63ff678b42bd48e97e72b987c1f151fb409bb8a5
frandichio <francisco.dichio11@gmail.com>
2023-04-12 20:11:00 -0300

    aaa

--- proyecto_django/views.py
+++ proyecto_django/views.py
@@ -11,3 +11,34 @@ def cursoFormulario(request):
 def Contact(request):
     contact_form = ContactForm()
     return render(request, 'contact/contact.html', {'form': contact_form})
+
+
+from .forms import ContactForm
+from .models import Contact
+
+def contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            message = form.cleaned_data['message']
+            contact_type = form.cleaned_data['contact_type']
+            subscription = form.cleaned_data['subscription']
+            
+            # Create and save Contact object
+            contact = Contact(
+                name=name,
+                email=email,
+                message=message,
+                contact_type=contact_type,
+                subscription=subscription
+            )
+            contact.save()
+            
+            return redirect('contact')  # Redirect to the contact page
+            
+    else:
+        form = ContactForm()
+        
+    return render(request, 'contact/contact.html', {'form': form})
