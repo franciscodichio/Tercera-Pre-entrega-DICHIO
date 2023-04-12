ba4a5b74a45260c0b971c2bba710673b5502d001
frandichio <francisco.dichio11@gmail.com>
2023-04-12 19:37:48 -0300

    ultimos cambios

--- contact/models.py
+++ contact/models.py
@@ -29,19 +29,12 @@ from django.db import models
 #     class Meta:
 #         app_label = 'contact'
 
-
-class Contact(models.Model):
-    fields = ['name', 'email', 'message', 'contact_type', 'subscription']
-    
-    class Meta:
-        app_label = 'contact'
-       
-
 options = [
     [0, 'Pedido de informacion'],
     [1, 'Queja por un producto'],
     [2, 'Felicitaciones']
 ]
+
 
 class Contact(models.Model):
     name = models.CharField(max_length=100, verbose_name='Nombre y Apellido')
@@ -50,4 +43,22 @@ class Contact(models.Model):
     contact_type = models.IntegerField(choices=options, verbose_name='Tipo de contacto')
     subscription = models.BooleanField(default=False, verbose_name='Subscribirme a correos informativos')
     crated_add = models.DateTimeField(auto_now_add=True, verbose_name='Fecha de envio')
+    
+    class Meta:
+        app_label = 'contact'
+       
+
+# options = [
+#     [0, 'Pedido de informacion'],
+#     [1, 'Queja por un producto'],
+#     [2, 'Felicitaciones']
+# ]
+
+# class Contact(models.Model):
+#     name = models.CharField(max_length=100, verbose_name='Nombre y Apellido')
+#     email = models.EmailField(verbose_name='Correo Electronico')
+#     message = models.TextField(verbose_name='Mensaje')
+#     contact_type = models.IntegerField(choices=options, verbose_name='Tipo de contacto')
+#     subscription = models.BooleanField(default=False, verbose_name='Subscribirme a correos informativos')
+#     crated_add = models.DateTimeField(auto_now_add=True, verbose_name='Fecha de envio')
     
--- proyecto_django/models.py
+++ proyecto_django/models.py
@@ -19,17 +19,26 @@ from django.db import models
 #     velocidad = models.IntegerField()
 #     fecha_creacion = models.DateField()
 
+
+class Contact(models.Model):
+     fields = ['name', 'email', 'message', 'contact_type', 'subscription']
+    
+     class Meta:
+         app_label = 'contact'
+
 options = [
-    [0, 'Pedido de informacion'],
-    [1, 'Queja por un producto'],
-    [2, 'Felicitaciones']
+     [0, 'Pedido de informacion'],
+     [1, 'Queja por un producto'],
+     [2, 'Felicitaciones']
 ]
 
 class Contact(models.Model):
-    name = models.CharField(max_length=100, verbose_name='Nombre y Apellido')
-    email = models.EmailField(verbose_name='Correo Electronico')
-    message = models.TextField(verbose_name='Mensaje')
-    contact_type = models.IntegerField(choices=options, verbose_name='Tipo de contacto')
-    subscription = models.BooleanField(default=False, verbose_name='Subscribirme a correos informativos')
-    crated_add = models.DateTimeField(auto_now_add=True, verbose_name='Fecha de envio')
+     name = models.CharField(max_length=100, verbose_name='Nombre y Apellido')
+     email = models.EmailField(verbose_name='Correo Electronico')
+     message = models.TextField(verbose_name='Mensaje')
+     contact_type = models.IntegerField(choices=options, verbose_name='Tipo de contacto')
+     subscription = models.BooleanField(default=False, verbose_name='Subscribirme a correos informativos')
+     crated_add = models.DateTimeField(auto_now_add=True, verbose_name='Fecha de envio')
+    
+    
     
